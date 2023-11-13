128724aba15c88c87752f8f985ae032071276468
dugu281 <102406954+dugu281@users.noreply.github.com>
2023-11-13 23:14:01 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -81,11 +81,11 @@ app.use(express.json());
 // app.use(require('./routes/seedRoutes'));
 
 
-app.use("/api", userRoutes);
-app.use("/api", fileRoutes);
-app.use("/api", productRoutes);
-app.use("/api", orderRoutes);
-app.use("/api", seedRoutes);
+app.use("/", userRoutes);
+app.use("/", fileRoutes);
+app.use("/", productRoutes);
+app.use("/", orderRoutes);
+app.use("/", seedRoutes);
 
 
 
