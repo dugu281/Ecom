df7ee98c081fca7f8177c82f34665a184aac87d0
dugu281 <102406954+dugu281@users.noreply.github.com>
2023-11-14 13:18:33 +0530

    Delete my-app/src directory

--- my-app/src/components/LoadingBox.js
+++
@@ -1,9 +0,0 @@
-import Spinner from 'react-bootstrap/Spinner';
-
-export default function LoadingBox() {
-  return (
-    <div class="spinner-border text-primary" role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
-  );
-}
